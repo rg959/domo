87167c1670d33b9e2b60e23cfbe98e3bf6799a15
Rayan GALOUZI <rayangalouzi@macbook-air-de-rayan.home>
2020-06-15 20:27:32 +0200

    first commit

---
+++ src/app/app.global.ts
@@ -0,0 +1,23 @@
+
+// Firebase setup
+export const FIREBASE: any = {
+    apiKey: "AIzaSyDMuOAkRskP7wUihnOpWk5Wn6EUKr9uT2A",
+    authDomain: "rara-65d2c.firebaseapp.com",
+    databaseURL: "https://rara-65d2c.firebaseio.com",
+    projectId: "rara-65d2c",
+    storageBucket: "rara-65d2c.appspot.com",
+    messagingSenderId: "453829015944",
+    appId: "1:453829015944:web:2f1ed87fe645e9fc9c223d",
+    measurementId: "G-9FWED35HVG"
+};
+
+// CLIENT ID for Google Login
+export const CLIENTID: any = {
+    android: '702901799858-uo8j9jm95lqbdan619l0mjhdp7t4tkjb.apps.googleusercontent.com',
+    ios: '702901799858-4b8sueaggl0uf9ans3vjhh5j3edle7l2.apps.googleusercontent.com'
+};
+
+// database
+export const DB: any = {
+    USER: 'authy/users/'
+};
